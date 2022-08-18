30c02f117fb47e311cba96a16c8d6ecaf44e3471
Ach. Faisol S. Arifin <faisolofficial99@gmail.com>
2022-08-18 19:43:15 +0700

    menggabungkan js ke index laporan

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -79,6 +79,67 @@
         </div>
     </div>
 
+    <!--Form Modal Tahun -->
+    <div class="modal fade text-left" id="modal-post-tahun" tabindex="-1" role="dialog"
+         aria-labelledby="myModalLabel1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Tambah Tahun</h5>
+                    <button type="button" class="close rounded-pill"
+                            data-bs-dismiss="modal" aria-label="Close">
+                        <i class="bi bi-x"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" id="formTahun">
+                        <div class="d-flex justify-content-center">
+                            <select class="form-select form-select-sm" name="tahun" id="tahun">
+                                @for ($y = date('Y'); $y > 2010; $y--)
+                                    <option value="{{ $y }}">{{ $y }}</option>
+                                @endfor
+                            </select>
+                            <button type="submit" class="btn btn-sm btn-primary ms-2">
+                                Simpan
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Form Modal Bulan -->
+    <div class="modal fade text-left" id="modal-post-bulan" tabindex="-1" role="dialog"
+         aria-labelledby="myModalLabel1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <form method="post" id="formBulan">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Tambah Periode</h5>
+                        <button type="button" class="close rounded-pill"
+                                data-bs-dismiss="modal" aria-label="Close">
+                            <i class="bi bi-x"></i>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        @include('laporan.formPeriode')
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">
+                            Close
+                        </button>
+                        <button type="submit" class="btn btn-sm btn-primary ml-1">
+                            Simpan
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
     <script>
         $(function () {
             $.ajaxSetup({
@@ -86,6 +147,146 @@
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
+
+            //-------------
+
+            //Fungsi Date
+            function date(date) {
+                date = date.split('-')
+                return date[2]+'/'+date[1]+'/'+date[0]
+            }
+            //Number Format
+            function currency(x) {
+                return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '.00';
+            }
+            // Simple Datatable
+            let table1 = document.querySelector('#table1');
+            let dataTable = new simpleDatatables.DataTable(table1);
+            $('.dataTable-selector').addClass('form-select-sm')
+            $('.dataTable-input').removeClass('dataTable-input').addClass('form-control').addClass('form-control-sm')
+
+            function showMenu() {
+
+                $.ajax({
+                    url: `{{ route('api.tahun') }}`,
+                    type: "GET",
+                    dataType: 'json',
+                    success: function(res) {
+                        if (Object.keys(res).length > 0) {
+                            option = '';
+                            dom = '<li class="sidebar-title d-flex justify-content-between align-items-center">PERIODE ' +
+                                '<button class="btn-y" data-bs-toggle="modal" data-bs-target="#modal-post-tahun">' +
+                                '<i class="bi bi-plus-circle"></i></button></li>';
+                            $.each(res.data, function(key, row) {
+                                option += `<option value="${row.id_thn}">${row.thn}</option>`;
+                                dom += `<li class="sidebar-item has-sub">`;
+                                dom += `<a href="#" class='sidebar-link' data-id="${row.id_thn}">
+                                    <i class="bi bi-grid-fill"></i>
+                                    <span>${row.thn}</span>
+                                </a>`;
+                                display = String(row.id_thn) == "{{Request()->tahun_id}}" ? 'block' : 'none';
+                                dom += `<ul class="submenu " style="display: ${display}">`;
+
+                                $.each(row.bulan, function(idx, bln) {
+                                    url = "{{ route('rep.kas', [':thn', ':bln']) }}";
+                                    url = url.replace(':thn', row.id_thn).replace(':bln', bln.id_periode);
+                                    dom += `<li class="submenu-item">
+                                        <a href="${url}">${bln.bulan}</a>
+                                    </li>`;
+                                })
+                                dom += `</ul>`;
+                                dom += `</li>`;
+                            });
+
+                            $('.menu').html(dom);
+                            $('#tahun-periode').html(option);
+                        }
+                    }
+                });
+            }
+
+            showMenu()
+
+            $('body').on('submit', '#formTahun', function(e) {
+                e.preventDefault()
+                $.ajax({
+                    url: "{{route('api.thn.s')}}",
+                    type: "POST",
+                    dataType: 'json',
+                    data: $('#formTahun').serialize(),
+                    success: function(res) {
+                        $('#modal-post-tahun').modal('hide');
+                        showMenu();
+                    },
+                });
+            });
+
+            $('body').on('click', '.delete-tahun', function(e) {
+                var _id = $(this).attr('data-id')
+                if (confirm("Data akan menghapus periode?")) {
+                    $.ajax({
+                        url: "{{ route('api.thn.d') }}",
+                        type: "DELETE",
+                        dataType: 'json',
+                        data: {
+                            kode: _id
+                        },
+                        success: function(res) {
+                            showMenu();
+                        },
+                    });
+                }
+            });
+
+            $('#modal-post-bulan').on('show.bs.modal', function(event) {
+                var _id = event.relatedTarget?.getAttribute('data-id')
+                $(this).find('#tahun-periode').val(_id)
+            })
+            $('#modal-post-bulan').on('hide.bs.modal', function(e) {
+                $(this).find('form').attr('id', 'formBulan');
+                $(this).find('.modal-title').text('Tambah Periode')
+
+                $('#formBulan').trigger("reset");
+                $('#formBulanUpdate').trigger("reset");
+            })
+
+            $('body').on('submit', '#formBulan', function(e) {
+                e.preventDefault()
+                $.ajax({
+                    url: "{{route('api.periode.s')}}",
+                    type: "POST",
+                    dataType: 'json',
+                    data: $('#formBulan').serialize(),
+                    success: function(res) {
+                        $('#modal-post-bulan').modal('hide');
+                        showMenu();
+                    },
+                });
+
+            });
+
+            $('body').on('contextmenu', '.sidebar-link', function(event) {
+                event.preventDefault();
+                var _id = $(this).attr('data-id');
+                var ctxMenu = $("#contextmenu");
+                ctxMenu.css("display", "block");
+                ctxMenu.css("left", (event.pageX - 10)+"px");
+                ctxMenu.css("top", (event.pageY - 10)+"px");
+                ctxMenu.find('li').attr('data-id', _id);
+            })
+            $('body').on('click', function(event) {
+                var ctxMenu = $("#contextmenu");
+                ctxMenu.css("display", "none");
+                ctxMenu.css("left", "");
+                ctxMenu.css("top", "");
+                ctxMenu.find('li').removeAttr('data-id');
+            })
+
+            $('body').on('input', '#saldo_awal', function() {
+                $('#sisa_saldo').val($(this).val())
+            });
+
+            //--------------------------------------------------------------------------
 
             $.ajax({
                 url: "{{route('api.bread')}}",
@@ -289,6 +490,5 @@
             showData()
 
         });
-
     </script>
 @endsection
