0108418322bfdc87e7915540fb9abf8a80f555c9
Ach. Faisol S. Arifin <faisolofficial99@gmail.com>
2022-08-23 21:04:42 +0700

    mengahapus cors tag meta

--- resources/views/templates/admin.blade.php
+++ resources/views/templates/admin.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="*">
     <title>Dashboard | Digital Report</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
