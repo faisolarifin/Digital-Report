98f8624f9c1c7ecb3eddba856f5d4c2ea03c7449
Ach. Faisol S. Arifin <faisolofficial99@gmail.com>
2022-10-23 15:40:55 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/success", function() {
+Route::post("/success", function() {
     return "success";
 });
